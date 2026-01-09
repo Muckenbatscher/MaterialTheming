92a24d8d04117ed6228b5ff31f6b22bdfe48c6e8
Muckenbatscher <christi.braeuner97@gmail.com>
2026-01-09 15:14:28 +0100

    make a separate test method for all test theme classes this makes debugging the tests a lot easier and they display nicer in the test result report

--- MaterialTheming.Tests/TonalSpotTests.cs
+++ MaterialTheming.Tests/TonalSpotTests.cs
@@ -7,19 +7,60 @@ namespace MaterialTheming.Tests;
 public sealed class TonalSpotTests
 {
     [TestMethod]
-    [DataRow(typeof(LightMode_NormalContrast_Spec2021))]
-    [DataRow(typeof(DarkMode_NormalContrast_Spec2021))]
-    [DataRow(typeof(LightMode_MediumContrast_Spec2021))]
-    [DataRow(typeof(DarkMode_MediumContrast_Spec2021))]
-    [DataRow(typeof(LightMode_HighContrast_Spec2021))]
-    [DataRow(typeof(DarkMode_HighContrast_Spec2021))]
-    [DataRow(typeof(LightMode_NormalContrast_Spec2025))]
-    [DataRow(typeof(DarkMode_MediumContrast_Spec2025))]
-    [DataRow(typeof(DarkMode_HighContrast_Spec2025))]
-    public void TonalSpot_ForType(Type testThemeType)
+    public void TonalSpot_LightMode_NormalContrast_Spec2021()
     {
-        var result = TestThemeTypeValidator.ValidateThemeType(testThemeType);
+        var result = ThemeValidator.ValidateThemeColors<LightMode_NormalContrast_Spec2021>();
+        Assert.IsTrue(result.IsValid, message: result.GetFailedValidationMessage());
+    }
+    [TestMethod]
+    public void TonalSpot_DarkMode_NormalContrast_Spec2021()
+    {
+        var result = ThemeValidator.ValidateThemeColors<DarkMode_NormalContrast_Spec2021>();
+        Assert.IsTrue(result.IsValid, message: result.GetFailedValidationMessage());
+    }
 
+    [TestMethod]
+    public void TonalSpot_LightMode_MediumContrast_Spec2021()
+    {
+        var result = ThemeValidator.ValidateThemeColors<LightMode_MediumContrast_Spec2021>();
+        Assert.IsTrue(result.IsValid, message: result.GetFailedValidationMessage());
+    }
+    [TestMethod]
+    public void TonalSpot_DarkMode_MediumContrast_Spec2021()
+    {
+        var result = ThemeValidator.ValidateThemeColors<DarkMode_MediumContrast_Spec2021>();
+        Assert.IsTrue(result.IsValid, message: result.GetFailedValidationMessage());
+    }
+
+    [TestMethod]
+    public void TonalSpot_LightMode_HighContrast_Spec2021()
+    {
+        var result = ThemeValidator.ValidateThemeColors<LightMode_HighContrast_Spec2021>();
+        Assert.IsTrue(result.IsValid, message: result.GetFailedValidationMessage());
+    }
+    [TestMethod]
+    public void TonalSpot_DarkMode_HighContrast_Spec2021()
+    {
+        var result = ThemeValidator.ValidateThemeColors<DarkMode_HighContrast_Spec2021>();
+        Assert.IsTrue(result.IsValid, message: result.GetFailedValidationMessage());
+    }
+
+    [TestMethod]
+    public void TonalSpot_LightMode_NormalContrast_Spec2025()
+    {
+        var result = ThemeValidator.ValidateThemeColors<LightMode_NormalContrast_Spec2025>();
+        Assert.IsTrue(result.IsValid, message: result.GetFailedValidationMessage());
+    }
+    [TestMethod]
+    public void TonalSpot_DarkMode_MediumContrast_Spec2025()
+    {
+        var result = ThemeValidator.ValidateThemeColors<DarkMode_MediumContrast_Spec2025>();
+        Assert.IsTrue(result.IsValid, message: result.GetFailedValidationMessage());
+    }
+    [TestMethod]
+    public void TonalSpot_DarkMode_HighContrast_Spec2025()
+    {
+        var result = ThemeValidator.ValidateThemeColors<DarkMode_HighContrast_Spec2025>();
         Assert.IsTrue(result.IsValid, message: result.GetFailedValidationMessage());
     }
 }
