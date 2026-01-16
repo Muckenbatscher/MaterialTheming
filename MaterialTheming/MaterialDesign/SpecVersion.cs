6ad94ebed80f705cac549ce3611348792c1955ff
Muckenbatscher <christi.braeuner97@gmail.com>
2026-01-16 16:27:34 +0100

    added xml doc for spec version

--- MaterialTheming/MaterialDesign/SpecVersion.cs
+++ MaterialTheming/MaterialDesign/SpecVersion.cs
@@ -1,7 +1,16 @@
 ﻿namespace MaterialTheming.MaterialDesign;
 
+/// <summary>
+/// The known Material 3 spec versions.
+/// </summary>
 public enum SpecVersion
 {
+    /// <summary>
+    /// The spec version released in 2021.
+    /// </summary>
     Spec2021,
+    /// <summary>
+    /// The spec version released in 2025.
+    /// </summary>
     Spec2025
 }
