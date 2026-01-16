d2f1fb7552f12763508443c1b9e5d0bfbbb7bb27
Muckenbatscher <christi.braeuner97@gmail.com>
2026-01-16 16:38:25 +0100

    added xml doc for the variant

--- MaterialTheming/Creation/ThemeBuilderBlocks/IWithVariantThemeBuilder.cs
+++ MaterialTheming/Creation/ThemeBuilderBlocks/IWithVariantThemeBuilder.cs
@@ -5,5 +5,10 @@ namespace MaterialTheming.Creation.ThemeBuilderBlocks;
 public interface IWithVariantThemeBuilder<TBuilder>
     where TBuilder : IBuildableThemeBuilder
 {
+    /// <summary>
+    /// Specify the variant that is used to build the <see cref="Theme" />.
+    /// </summary>
+    /// <param name="variant">The variant to use.</param>
+    /// <returns>The <typeparamref name="TBuilder"/> with the specified variant.</returns>
     TBuilder WithVariant(Variant variant);
 }
