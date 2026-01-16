d05d43572d7efb3197679802ed030912e1673938
Muckenbatscher <christi.braeuner97@gmail.com>
2026-01-16 11:42:38 +0100

    added xml doc to the theme and theme colors the two types that are generally the output of the library

--- MaterialTheming/ThemeColors.cs
+++ MaterialTheming/ThemeColors.cs
@@ -1,69 +1,219 @@
 ﻿using MaterialTheming.ColorDefinitions;
 
 namespace MaterialTheming;
-
+/// <summary>
+/// A representation of the color roles defined in the Material 3 specification. <br/>
+/// <see href="https://m3.material.io/styles/color/roles"/>
+/// </summary>
 public class ThemeColors
 {
     //Primary
+    /// <summary>
+    /// The color assigned to the <b>Primary</b> Material 3 color role.
+    /// </summary>
     public RgbColor Primary { get; set; } = RgbColor.Empty;
+    /// <summary>
+    /// The color assigned to the <b>On Primary</b> Material 3 color role.
+    /// </summary>
     public RgbColor OnPrimary { get; set; } = RgbColor.Empty;
+    /// <summary>
+    /// The color assigned to the <b>Primary Container</b> Material 3 color role.
+    /// </summary>
     public RgbColor PrimaryContainer { get; set; } = RgbColor.Empty;
+    /// <summary>
+    /// The color assigned to the <b>On Primary Container</b> Material 3 color role.
+    /// </summary>
     public RgbColor OnPrimaryContainer { get; set; } = RgbColor.Empty;
     //Secondary
+    /// <summary>
+    /// The color assigned to the <b>Secondary</b> Material 3 color role.
+    /// </summary>
     public RgbColor Secondary { get; set; } = RgbColor.Empty;
+    /// <summary>
+    /// The color assigned to the <b>On Secondary</b> Material 3 color role.
+    /// </summary>
     public RgbColor OnSecondary { get; set; } = RgbColor.Empty;
+    /// <summary>
+    /// The color assigned to the <b>Secondary Container</b> Material 3 color role.
+    /// </summary>
     public RgbColor SecondaryContainer { get; set; } = RgbColor.Empty;
+    /// <summary>
+    /// The color assigned to the <b>On Secondary Container</b> Material 3 color role.
+    /// </summary>
     public RgbColor OnSecondaryContainer { get; set; } = RgbColor.Empty;
     //Tertiary
+    /// <summary>
+    /// The color assigned to the <b>Tertiary</b> Material 3 color role.
+    /// </summary>
     public RgbColor Tertiary { get; set; } = RgbColor.Empty;
+    /// <summary>
+    /// The color assigned to the <b>On Tertiary</b> Material 3 color role.
+    /// </summary>
     public RgbColor OnTertiary { get; set; } = RgbColor.Empty;
+    /// <summary>
+    /// The color assigned to the <b>Tertiary Container</b> Material 3 color role.
+    /// </summary>
     public RgbColor TertiaryContainer { get; set; } = RgbColor.Empty;
+    /// <summary>
+    /// The color assigned to the <b>On Tertiary Container</b> Material 3 color role.
+    /// </summary>
     public RgbColor OnTertiaryContainer { get; set; } = RgbColor.Empty;
     //Error
+    /// <summary>
+    /// The color assigned to the <b>Error</b> Material 3 color role.
+    /// </summary>
     public RgbColor Error { get; set; } = RgbColor.Empty;
+    /// <summary>
+    /// The color assigned to the <b>On Error</b> Material 3 color role.
+    /// </summary>
     public RgbColor OnError { get; set; } = RgbColor.Empty;
+    /// <summary>
+    /// The color assigned to the <b>Error Container</b> Material 3 color role.
+    /// </summary>
     public RgbColor ErrorContainer { get; set; } = RgbColor.Empty;
+    /// <summary>
+    /// The color assigned to the <b>On Error Container</b> Material 3 color role.
+    /// </summary>
     public RgbColor OnErrorContainer { get; set; } = RgbColor.Empty;
     //Surface
+    /// <summary>
+    /// The color assigned to the <b>Surface</b> Material 3 color role.
+    /// </summary>
     public RgbColor Surface { get; set; } = RgbColor.Empty;
+    /// <summary>
+    /// The color assigned to the <b>Surface Variant</b> Material 3 color role.
+    /// </summary>
     public RgbColor SurfaceVariant { get; set; } = RgbColor.Empty;
+    /// <summary>
+    /// The color assigned to the <b>On Surface</b> Material 3 color role.
+    /// </summary>
     public RgbColor OnSurface { get; set; } = RgbColor.Empty;
+    /// <summary>
+    /// The color assigned to the <b>On Surface Variant</b> Material 3 color role.
+    /// </summary>
     public RgbColor OnSurfaceVariant { get; set; } = RgbColor.Empty;
+    /// <summary>
+    /// The color assigned to the <b>Surface Dim</b> Material 3 color role.
+    /// </summary>
     public RgbColor SurfaceDim { get; set; } = RgbColor.Empty;
+    /// <summary>
+    /// The color assigned to the <b>Surface Bright</b> Material 3 color role.
+    /// </summary>
     public RgbColor SurfaceBright { get; set; } = RgbColor.Empty;
+    /// <summary>
+    /// The color assigned to the <b>Surface Tint</b> Material 3 color role.
+    /// </summary>
     public RgbColor SurfaceTint { get; set; } = RgbColor.Empty;
     //Background
+    /// <summary>
+    /// The color assigned to the <b>Background</b> Material 3 color role.
+    /// </summary>
     public RgbColor Background { get; set; } = RgbColor.Empty;
+    /// <summary>
+    /// The color assigned to the <b>On Background</b> Material 3 color role.
+    /// </summary>
     public RgbColor OnBackground { get; set; } = RgbColor.Empty;
     //Outline
+    /// <summary>
+    /// The color assigned to the <b>Outline</b> Material 3 color role.
+    /// </summary>
     public RgbColor Outline { get; set; } = RgbColor.Empty;
+    /// <summary>
+    /// The color assigned to the <b>Outline Variant</b> Material 3 color role.
+    /// </summary>
     public RgbColor OutlineVariant { get; set; } = RgbColor.Empty;
     //Shadow
+    /// <summary>
+    /// The color assigned to the <b>Shadow</b> Material 3 color role.
+    /// </summary>
     public RgbColor Shadow { get; set; } = RgbColor.Empty;
+    /// <summary>
+    /// The color assigned to the <b>Scrim</b> Material 3 color role.
+    /// </summary>
     public RgbColor Scrim { get; set; } = RgbColor.Empty;
     //Inverse
+    /// <summary>
+    /// The color assigned to the <b>Inverse Surface</b> Material 3 color role.
+    /// </summary>
     public RgbColor InverseSurface { get; set; } = RgbColor.Empty;
+    /// <summary>
+    /// The color assigned to the <b>Inverse On Surface</b> Material 3 color role.
+    /// </summary>
     public RgbColor InverseOnSurface { get; set; } = RgbColor.Empty;
+    /// <summary>
+    /// The color assigned to the <b>Inverse Primary</b> Material 3 color role.
+    /// </summary>
     public RgbColor InversePrimary { get; set; } = RgbColor.Empty;
     //Primary Fixed
+    /// <summary>
+    /// The color assigned to the <b>Primary Fixed</b> Material 3 color role.
+    /// </summary>
     public RgbColor PrimaryFixed { get; set; } = RgbColor.Empty;
+    /// <summary>
+    /// The color assigned to the <b>On Primary Fixed</b> Material 3 color role.
+    /// </summary>
     public RgbColor OnPrimaryFixed { get; set; } = RgbColor.Empty;
+    /// <summary>
+    /// The color assigned to the <b>Primary Fixed Dim</b> Material 3 color role.
+    /// </summary>
     public RgbColor PrimaryFixedDim { get; set; } = RgbColor.Empty;
+    /// <summary>
+    /// The color assigned to the <b>On Primary Fixed Variant</b> Material 3 color role.
+    /// </summary>
     public RgbColor OnPrimaryFixedVariant { get; set; } = RgbColor.Empty;
     //Secondary Fixed
+    /// <summary>
+    /// The color assigned to the <b>Secondary Fixed</b> Material 3 color role.
+    /// </summary>
     public RgbColor SecondaryFixed { get; set; } = RgbColor.Empty;
+    /// <summary>
+    /// The color assigned to the <b>On Secondary Fixed</b> Material 3 color role.
+    /// </summary>
     public RgbColor OnSecondaryFixed { get; set; } = RgbColor.Empty;
+    /// <summary>
+    /// The color assigned to the <b>Secondary Fixed Dim</b> Material 3 color role.
+    /// </summary>
     public RgbColor SecondaryFixedDim { get; set; } = RgbColor.Empty;
+    /// <summary>
+    /// The color assigned to the <b>On Secondary Fixed Variant</b> Material 3 color role.
+    /// </summary>
     public RgbColor OnSecondaryFixedVariant { get; set; } = RgbColor.Empty;
     //Tertiary Fixed
+    /// <summary>
+    /// The color assigned to the <b>Tertiary Fixed</b> Material 3 color role.
+    /// </summary>
     public RgbColor TertiaryFixed { get; set; } = RgbColor.Empty;
+    /// <summary>
+    /// The color assigned to the <b>On Tertiary Fixed</b> Material 3 color role.
+    /// </summary>
     public RgbColor OnTertiaryFixed { get; set; } = RgbColor.Empty;
+    /// <summary>
+    /// The color assigned to the <b>Tertiary Fixed Dim</b> Material 3 color role.
+    /// </summary>
     public RgbColor TertiaryFixedDim { get; set; } = RgbColor.Empty;
+    /// <summary>
+    /// The color assigned to the <b>On Tertiary Fixed Variant</b> Material 3 color role.
+    /// </summary>
     public RgbColor OnTertiaryFixedVariant { get; set; } = RgbColor.Empty;
     //Surface Container
+    /// <summary>
+    /// The color assigned to the <b>Surface Container Lowest</b> Material 3 color role.
+    /// </summary>
     public RgbColor SurfaceContainerLowest { get; set; } = RgbColor.Empty;
+    /// <summary>
+    /// The color assigned to the <b>Surface Container Low</b> Material 3 color role.
+    /// </summary>
     public RgbColor SurfaceContainerLow { get; set; } = RgbColor.Empty;
+    /// <summary>
+    /// The color assigned to the <b>Surface Container</b> Material 3 color role.
+    /// </summary>
     public RgbColor SurfaceContainer { get; set; } = RgbColor.Empty;
+    /// <summary>
+    /// The color assigned to the <b>Surface Container High</b> Material 3 color role.
+    /// </summary>
     public RgbColor SurfaceContainerHigh { get; set; } = RgbColor.Empty;
+    /// <summary>
+    /// The color assigned to the <b>Surface Container Highest</b> Material 3 color role.
+    /// </summary>
     public RgbColor SurfaceContainerHighest { get; set; } = RgbColor.Empty;
 }
