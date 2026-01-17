c5eb99cec79436f5641b1d025685f760a0e31e55
Muckenbatscher <christi.braeuner97@gmail.com>
2026-01-17 20:34:17 +0100

    added xml doc for material theme builder json content

--- MaterialTheming/Creation/ThemeBuilderBlocks/IWithMaterialThemeBuilderJsonThemeBuilder.cs
+++ MaterialTheming/Creation/ThemeBuilderBlocks/IWithMaterialThemeBuilderJsonThemeBuilder.cs
@@ -3,6 +3,16 @@
 public interface IWithMaterialThemeBuilderJsonThemeBuilder<TBuilder>
     where TBuilder : IBuildableThemeBuilder
 {
+    /// <summary>
+    /// Specify the <see href="https://material-foundation.github.io/material-theme-builder/">material theme builder</see> JSON content that is used to build the <see cref="Theme" />.
+    /// </summary>
+    /// <param name="materialThemeBuilderJson">The JSON content of the exported theme from <see href="https://material-foundation.github.io/material-theme-builder/"/></param>
+    /// <returns>The <typeparamref name="TBuilder"/> with the material theme builder JSON content.</returns>
     TBuilder WithMaterialThemeBuilderJsonContent(string materialThemeBuilderJson);
+    /// <summary>
+    /// Specify the <see href="https://material-foundation.github.io/material-theme-builder/">material theme builder</see> JSON content that is used to build the <see cref="Theme" />.
+    /// </summary>
+    /// <param name="materialThemeBuilderJsonFilePath">The path to the JSON file that was exported from <see href="https://material-foundation.github.io/material-theme-builder/"/></param>
+    /// <returns>The <typeparamref name="TBuilder"/> with the material theme builder JSON content of the specified file.</returns>
     TBuilder WithMaterialThemeBuilderJsonFilePath(string materialThemeBuilderJsonFilePath);
 }
