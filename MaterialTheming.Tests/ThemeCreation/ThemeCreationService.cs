6f007977a79c071baa577a60f8bb0ad281c9945c
Muckenbatscher <christi.braeuner97@gmail.com>
2026-02-19 14:31:12 +0100

    added differentiation in ThemeCreationService to create the ThemeColors with two source colors when interface is matched

--- MaterialTheming.Tests/ThemeCreation/ThemeCreationService.cs
+++ MaterialTheming.Tests/ThemeCreation/ThemeCreationService.cs
@@ -12,8 +12,14 @@ internal class ThemeCreationService
     }
     public static ThemeColors CreateThemeColors(IThemeCreationParameters parameters)
     {
+        IColorPaletteThemeBuilder builder;
+        if (parameters is IThemeCreationParametersSecondSourceColor secondSourceColorCreationParameters)
+            builder = ThemeBuilder.CreateFromSourceColors(parameters.SourceColor, secondSourceColorCreationParameters.SecondSourceColor);
+        else
+            builder = ThemeBuilder.CreateFromSourceColor(parameters.SourceColor);
+
         var mode = parameters.IsDark ? ThemeMode.Dark : ThemeMode.Light;
-        return ThemeBuilder.CreateFromSourceColor(parameters.SourceColor)
+        return builder
             .WithMode(mode)
             .WithContrastLevel(parameters.ContrastLevelValue)
             .WithVariant(parameters.Variant)
