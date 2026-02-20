5c8162e4f7f1073af9cad17b060bb62ae8f06fdf
Muckenbatscher <christi.braeuner97@gmail.com>
2026-02-20 01:16:26 +0100

    remove unnecessary tests with no test cases for the cmf variant make tests fail again when no test theme for this permutation was found

--- MaterialTheming.Tests/CmfTests.cs
+++ MaterialTheming.Tests/CmfTests.cs
@@ -4,4 +4,17 @@
 public class CmfTests : TestThemeTests
 {
     protected override Variant ThemeVariant => Variant.CMF;
+
+    public override void LightMode_NormalContrast_Spec2021() { }
+    public override void DarkMode_NormalContrast_Spec2021() { }
+    public override void LightMode_MediumContrast_Spec2021() { }
+    public override void DarkMode_MediumContrast_Spec2021() { }
+    public override void LightMode_HighContrast_Spec2021() { }
+    public override void DarkMode_HighContrast_Spec2021() { }
+    public override void LightMode_NormalContrast_Spec2025() { }
+    public override void DarkMode_NormalContrast_Spec2025() { }
+    public override void LightMode_MediumContrast_Spec2025() { }
+    public override void DarkMode_MediumContrast_Spec2025() { }
+    public override void LightMode_HighContrast_Spec2025() { }
+    public override void DarkMode_HighContrast_Spec2025() { }
 }
--- MaterialTheming.Tests/TestThemeTests.cs
+++ MaterialTheming.Tests/TestThemeTests.cs
@@ -26,8 +26,12 @@ public abstract class TestThemeTests
         }
         if (!discoveredAny)
         {
-            TestContext.WriteLine($"No {nameof(ITestTheme)} implementations were found");
-            TestContext.WriteLine($"Variant={ThemeVariant}, Mode={mode}, ContrastLevel={contrastLevel}, specVersion={specVersion}");
+            var messageLines = new List<string>()
+            {
+                $"No {nameof(ITestTheme)} implementations were found",
+                $"Variant={ThemeVariant}, Mode={mode}, ContrastLevel={contrastLevel}, specVersion={specVersion}"
+            };
+            Assert.Fail(string.Join(Environment.NewLine, messageLines));
             return;
         }
 
@@ -37,96 +41,96 @@ public abstract class TestThemeTests
     }
 
     [TestMethod]
-    public void LightMode_NormalContrast_Spec2021()
+    public virtual void LightMode_NormalContrast_Spec2021()
     {
         TestDiscoveredThemes(ThemeMode.Light, ContrastLevel.Normal, SpecVersion.Spec2021);
     }
     [TestMethod]
-    public void DarkMode_NormalContrast_Spec2021()
+    public virtual void DarkMode_NormalContrast_Spec2021()
     {
         TestDiscoveredThemes(ThemeMode.Dark, ContrastLevel.Normal, SpecVersion.Spec2021);
     }
 
     [TestMethod]
-    public void LightMode_MediumContrast_Spec2021()
+    public virtual void LightMode_MediumContrast_Spec2021()
     {
         TestDiscoveredThemes(ThemeMode.Light, ContrastLevel.Medium, SpecVersion.Spec2021);
     }
     [TestMethod]
-    public void DarkMode_MediumContrast_Spec2021()
+    public virtual void DarkMode_MediumContrast_Spec2021()
     {
         TestDiscoveredThemes(ThemeMode.Dark, ContrastLevel.Medium, SpecVersion.Spec2021);
     }
 
     [TestMethod]
-    public void LightMode_HighContrast_Spec2021()
+    public virtual void LightMode_HighContrast_Spec2021()
     {
         TestDiscoveredThemes(ThemeMode.Light, ContrastLevel.High, SpecVersion.Spec2021);
     }
     [TestMethod]
-    public void DarkMode_HighContrast_Spec2021()
+    public virtual void DarkMode_HighContrast_Spec2021()
     {
         TestDiscoveredThemes(ThemeMode.Dark, ContrastLevel.High, SpecVersion.Spec2021);
     }
 
     [TestMethod]
-    public void LightMode_NormalContrast_Spec2025()
+    public virtual void LightMode_NormalContrast_Spec2025()
     {
         TestDiscoveredThemes(ThemeMode.Light, ContrastLevel.Normal, SpecVersion.Spec2025);
     }
     [TestMethod]
-    public void DarkMode_NormalContrast_Spec2025()
+    public virtual void DarkMode_NormalContrast_Spec2025()
     {
         TestDiscoveredThemes(ThemeMode.Dark, ContrastLevel.Normal, SpecVersion.Spec2025);
     }
     [TestMethod]
-    public void LightMode_MediumContrast_Spec2025()
+    public virtual void LightMode_MediumContrast_Spec2025()
     {
         TestDiscoveredThemes(ThemeMode.Light, ContrastLevel.Medium, SpecVersion.Spec2025);
     }
     [TestMethod]
-    public void DarkMode_MediumContrast_Spec2025()
+    public virtual void DarkMode_MediumContrast_Spec2025()
     {
         TestDiscoveredThemes(ThemeMode.Dark, ContrastLevel.Medium, SpecVersion.Spec2025);
     }
     [TestMethod]
-    public void LightMode_HighContrast_Spec2025()
+    public virtual void LightMode_HighContrast_Spec2025()
     {
         TestDiscoveredThemes(ThemeMode.Light, ContrastLevel.High, SpecVersion.Spec2025);
     }
     [TestMethod]
-    public void DarkMode_HighContrast_Spec2025()
+    public virtual void DarkMode_HighContrast_Spec2025()
     {
         TestDiscoveredThemes(ThemeMode.Dark, ContrastLevel.High, SpecVersion.Spec2025);
     }
 
     [TestMethod]
-    public void LightMode_NormalContrast_Spec2026()
+    public virtual void LightMode_NormalContrast_Spec2026()
     {
         TestDiscoveredThemes(ThemeMode.Light, ContrastLevel.Normal, SpecVersion.Spec2026);
     }
     [TestMethod]
-    public void DarkMode_NormalContrast_Spec2026()
+    public virtual void DarkMode_NormalContrast_Spec2026()
     {
         TestDiscoveredThemes(ThemeMode.Dark, ContrastLevel.Normal, SpecVersion.Spec2026);
     }
     [TestMethod]
-    public void LightMode_MediumContrast_Spec2026()
+    public virtual void LightMode_MediumContrast_Spec2026()
     {
         TestDiscoveredThemes(ThemeMode.Light, ContrastLevel.Medium, SpecVersion.Spec2026);
     }
     [TestMethod]
-    public void DarkMode_MediumContrast_Spec2026()
+    public virtual void DarkMode_MediumContrast_Spec2026()
     {
         TestDiscoveredThemes(ThemeMode.Dark, ContrastLevel.Medium, SpecVersion.Spec2026);
     }
     [TestMethod]
-    public void LightMode_HighContrast_Spec2026()
+    public virtual void LightMode_HighContrast_Spec2026()
     {
         TestDiscoveredThemes(ThemeMode.Light, ContrastLevel.High, SpecVersion.Spec2026);
     }
     [TestMethod]
-    public void DarkMode_HighContrast_Spec2026()
+    public virtual void DarkMode_HighContrast_Spec2026()
     {
         TestDiscoveredThemes(ThemeMode.Dark, ContrastLevel.High, SpecVersion.Spec2026);
     }
