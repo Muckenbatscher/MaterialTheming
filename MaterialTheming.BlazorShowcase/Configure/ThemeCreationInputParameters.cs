dac5fa3868dab10ccc8ef36d6767439ac6161cde
Muckenbatscher <christi.braeuner97@gmail.com>
2026-04-02 22:01:43 +0200

    add BlazorShowcase to solution
    perform required adjustments and add project reference to MaterialTheming library instead of nuget reference

---
+++ MaterialTheming.BlazorShowcase/Configure/ThemeCreationInputParameters.cs
@@ -0,0 +1,12 @@
+﻿using MaterialTheming;
+
+namespace MaterialThemingShowcase.Configure;
+
+public class ThemeCreationInputParameters
+{
+    public required RgbColor SourceColor { get; set; }
+    public Variant Variant { get; set; }
+    public ThemeMode Mode { get; set; }
+    public ContrastLevel Contrast { get; set; }
+    public SpecVersion SpecVersion { get; set; }
+}
