0d5ed296be5448c3637966b0b9828edfcf109bc8
Muckenbatscher <christi.braeuner97@gmail.com>
2026-07-28 15:28:06 +0200

    Add a colorpicker for the second source color
    Show the colorpicker based on the selction for the variant.
    Currently only the CMF variant has the ability to accept a second source
    color.

--- MaterialTheming.BlazorShowcase/Configure/ThemeCreationInputParameters.cs
+++ MaterialTheming.BlazorShowcase/Configure/ThemeCreationInputParameters.cs
@@ -2,9 +2,10 @@
 
 namespace MaterialTheming.BlazorShowcase.Configure;
 
-public class ThemeCreationInputParameters
+public record ThemeCreationInputParameters
 {
     public required RgbColor SourceColor { get; set; }
+    public RgbColor? SecondSourceColor { get; set; }
     public Variant Variant { get; set; }
     public ThemeMode Mode { get; set; }
     public ContrastLevel Contrast { get; set; }
