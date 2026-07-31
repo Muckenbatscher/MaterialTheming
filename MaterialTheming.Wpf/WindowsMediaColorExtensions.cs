bec418ae54ee432dd4b99e2f4eca52ea7bfe62b0
Muckenbatscher <christi.braeuner97@gmail.com>
2026-07-31 13:19:40 +0200

    added XML comments to public types and methods

--- MaterialTheming.Wpf/WindowsMediaColorExtensions.cs
+++ MaterialTheming.Wpf/WindowsMediaColorExtensions.cs
@@ -2,10 +2,18 @@ using System.Windows.Media;
 
 namespace MaterialTheming;
 
+/// <summary>
+/// Provides extension methods for converting between 
+/// <see cref="RgbColor"/>, <see cref="HctColor"/>, and the native <see cref="Color"/>.
+/// </summary>
 public static class WindowsMediaColorExtensions
 {
     extension(RgbColor rgbColor)
     {
+        /// <summary>
+        /// Converts the <see cref="RgbColor"/> to a native <see cref="Color"/>.
+        /// </summary>
+        /// <returns></returns>
         public Color ToColor()
         {
             return Color.FromRgb(rgbColor.Red, rgbColor.Green, rgbColor.Blue);
@@ -14,6 +22,10 @@ public static class WindowsMediaColorExtensions
 
     extension(Color color)
     {
+        /// <summary>
+        /// Converts the native <see cref="Color"/> to a <see cref="RgbColor"/>.
+        /// </summary>
+        /// <returns></returns>
         public RgbColor ToRgbColor()
         {
             return RgbColor.FromRgb(color.R, color.G, color.B);
@@ -22,11 +34,19 @@ public static class WindowsMediaColorExtensions
 
     extension(HctColor hctColor)
     {
+        /// <summary>
+        /// Converts the <see cref="HctColor"/> to a native <see cref="Color"/>.
+        /// </summary>
+        /// <returns></returns>
         public Color ToColor() => hctColor.ToRgbColor().ToColor();
     }
 
     extension(Color color)
     {
+        /// <summary>
+        /// Converts the native <see cref="Color"/> to a <see cref="HctColor"/>.
+        /// </summary>
+        /// <returns></returns>
         public HctColor ToHctColor() => color.ToRgbColor().ToHct();
     }
 }
