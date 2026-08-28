722d1a601f25092fa5817517f82d58ea55884931
Muckenbatscher <christi.braeuner97@gmail.com>
2026-08-28 11:56:35 +0200

    Use global NuGet Package Metadata
    Also add XML Doc Comments to the MudColor extension methods

--- MaterialTheming.MudBlazor/MudBlazorColorExtensions.cs
+++ MaterialTheming.MudBlazor/MudBlazorColorExtensions.cs
@@ -2,10 +2,18 @@ using MudBlazor.Utilities;
 
 namespace MaterialTheming;
 
+/// <summary>
+/// Provides extension methods for converting between 
+/// <see cref="RgbColor"/>, <see cref="HctColor"/>, and <see cref="MudColor"/>.
+/// </summary>
 public static class MudBlazorColorExtensions
 {
     extension(RgbColor rgbColor)
     {
+        /// <summary>
+        /// Converts the <see cref="RgbColor"/> to a <see cref="MudColor"/>.
+        /// </summary>
+        /// <returns>A <see cref="MudColor"/> with the same RGB values.</returns>
         public MudColor ToMudColor()
         {
             return new MudColor(r: rgbColor.Red, g: rgbColor.Green, b: rgbColor.Blue, a: byte.MaxValue);
@@ -14,15 +22,31 @@ public static class MudBlazorColorExtensions
 
     extension(HctColor hctColor)
     {
-        public MudColor ToColor() => hctColor.ToRgbColor().ToMudColor();
+        /// <summary>
+        /// Converts the <see cref="HctColor"/> to a <see cref="MudColor"/>.
+        /// </summary>
+        /// <returns>A <see cref="MudColor"/> converted to RGB color space.</returns>
+        public MudColor ToMudColor() => hctColor.ToRgbColor().ToMudColor();
     }
 
     extension(MudColor color)
     {
+        /// <summary>
+        /// Converts the <see cref="MudColor"/> to a <see cref="RgbColor"/>.
+        /// </summary>
+        /// <returns>A <see cref="RgbColor"/> with the same RGB values.</returns>
         public RgbColor ToRgbColor()
         {
             return RgbColor.FromRgb(color.R, color.G, color.B);
         }
-        public HctColor ToHctColor() => color.ToRgbColor().ToHct();
+
+        /// <summary>
+        /// Converts the <see cref="MudColor"/> to a <see cref="HctColor"/>.
+        /// </summary>
+        /// <returns>A <see cref="HctColor"/> converted from RGB color space.</returns>
+        public HctColor ToHctColor()
+        {
+            return color.ToRgbColor().ToHct();
+        }
     }
 }
